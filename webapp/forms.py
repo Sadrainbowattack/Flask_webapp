fecfc5e619cddd19bd47dee70e97e9950c182553
Max Glinskii <maxglinsky@gmail.com>
2022-04-01 12:35:47 +0400

    login procedure was updated

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import BooleanField, StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()], render_kw={"class": "form-control"})
     password = PasswordField('Password', validators=[DataRequired()], render_kw={"class": "form-control"})
     submit = SubmitField('OK', render_kw={"class":"btn btn-primary"})
+    remember_me = BooleanField('Remember me', default=True,
+            render_kw={"class": "form-check-input"})
